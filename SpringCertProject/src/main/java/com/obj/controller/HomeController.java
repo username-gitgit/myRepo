a9b20983eb1efa937c12d75b65f065cf3ea60e23
usergit <gusgh2435@gmail.com>
2023-04-19 15:02:13 +0900

    Create New Project

---
+++ SpringCertProject/src/main/java/com/obj/controller/HomeController.java
@@ -0,0 +1,51 @@
+package com.obj.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@GetMapping("LogIn")
+	public String loginPage() {
+		logger.info("Welcome Login!");
+		return "LogIn";
+	}
+	
+	@GetMapping("SignUp")
+	public String SignUpPage() {
+		logger.info("Welcome SignUp!");
+		return "SignUp";
+	}
+}
---
+++ SpringCertProject/src/main/webapp/WEB-INF/views/SignUp.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원가입</title>
+<link rel="stylesheet" href="../resources/css/mystyle.css">
+</head>
+<body>
+<form action="insert" method="post">
+<h1>회원가입</h1>
+이메일<input type="text" name="email" id="email"><br>
+비밀번호<input type="text" name="password" id="password"><br>
+비밀번호 확인<input type="text" name="password" id="password"><br>
+이름<input type="text" name="name" id="name"><br>
+주민등록번호<input type="text" name="residentid" id="residentid"><br>
+휴대폰 번호<input type="text" name="phone" id="phone"><br>
+<input type="submit" value="회원 가입">
+</form>
+</body>
+</html>
